d5947ee1e829b00632fa3ee8ccbfeab1b412c999
Samuel_Guillot <samuelguillot75@gmail.com>
2024-10-10 15:23:11 +0200

    correction: mariadb

--- modules/mariadb.sh
+++ modules/mariadb.sh
@@ -1,4 +1,13 @@
 sudo apt install -y mariadb-server mariadb-client
 sudo systemctl start mariadb
 sudo systemctl status mariadb
-sudo mysql_secure_installation
+
+sudo mysql --execute="UPDATE mysql.user SET Password=PASSWORD('user') WHERE User='root';"
+sudo mysql --execute="FLUSH PRIVILEGES;"
+sudo mysql --execute="DELETE FROM mysql.user WHERE User='';"
+sudo mysql --execute="DROP DATABASE IF EXISTS test;"
+sudo mysql --execute="FLUSH PRIVILEGES;"
+
+sudo mysql --execute="CREATE USER 'user'@'localhost' IDENTIFIED BY 'user';"
+sudo mysql --execute="GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost' WITH GRANT OPTION;"
+sudo mysql --execute="FLUSH PRIVILEGES;"
