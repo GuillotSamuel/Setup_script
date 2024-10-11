bc8ea51f3ab35e67a873df4b513a9ac5be7833e6
Samuel_Guillot <samuelguillot75@gmail.com>
2024-10-11 14:36:50 +0200

    correction

--- modules/zsh_ohmyzsh.sh
+++ modules/zsh_ohmyzsh.sh
@@ -1,3 +1,3 @@
 sudo apt install -y zsh
 sudo chsh -s $(which zsh)
-sudo sh -c "$(wget -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
